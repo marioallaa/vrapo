e364be57204cf3a0f7d83e8d609b296f7f90e66c
Mario Alla <mario.alla9.0@gmail.com>
2020-09-25 00:53:13 +0200

    vrapimi i pare

---
+++ vrapo.py
@@ -0,0 +1,30 @@
+
+import sys, os, magic
+fileToRun = ''
+for l in sys.argv[1:]:
+    fileToRun+= ' ' + l
+
+try: 
+    t = magic.from_file(fileToRun[1:], mime=True)
+except Exception as e:
+    t = ''
+if 'image' in t:
+    os.system('eog' + fileToRun)
+elif '/pdf' in t:
+    os.system('evince' + fileToRun)
+elif t == 'text/x-python':
+    os.system('python3' + fileToRun)
+elif t == 'text/plain':
+    t = fileToRun.split('.')[-1]
+    if t == 'py':
+        os.system('python3' + fileToRun)
+    elif t == 'js' or t == 'ts':
+        os.system('node' + fileToRun)
+elif os.path.isfile('./package.json'):
+    os.system('npm run' + fileToRun) 
+else: 
+    print('filetype: ', t, 'unsuported')
+
+
+
+
